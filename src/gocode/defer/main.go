6c73d84a6469495fe8d8e2880db42281145d862b
Janiak <69315011+Janiakondacloud@users.noreply.github.com>
2022-11-14 00:31:49 +0800

    day3

--- src/gocode/defer/main.go
+++ src/gocode/defer/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+// 延迟执行 defer先定义后执行
+func main() {
+	fmt.Println("start")
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	defer fmt.Println(3)
+	fmt.Println("end")
 }
--- src/gocode/func/main.go
+++ src/gocode/func/main.go
@@ -1,5 +1,43 @@
 package main
 
+import "fmt"
+
+func intSum(a int, b int) int {
+	return a + b
+}
+func intSum1(a int, b int) (ret int) {
+	ret = a + b
+	return
+}
+func intSum2(args ...int) (ret int) {
+	for _, arg := range args {
+		ret += arg
+	}
+	return
+}
+func calc(a, b int) (sum, sub int) {
+	sum = a + b
+	sub = a - b
+	return
+}
+func calc1(a, b int, op func(int, int) int) int {
+	return op(a, b)
+}
+func a() func() {
+	name := "沙河帅哥"
+	return func() {
+		fmt.Println("hello 沙河小王子" + name)
+	}
+}
 func main() {
-	$END$
+	fmt.Println(intSum(2, 5))
+	fmt.Println(intSum1(3, 5))
+	fmt.Println(intSum2(3, 5, 2, 1, 5))
+	sum, sub := calc(100, 200)
+	fmt.Println(sum, sub)
+	fmt.Println(intSum1(intSum(1, 2), 3))
+	fmt.Println(calc1(2, 5, intSum))
+	r := a()
+	//闭包=函数+外层变量的引用
+	r()
 }
